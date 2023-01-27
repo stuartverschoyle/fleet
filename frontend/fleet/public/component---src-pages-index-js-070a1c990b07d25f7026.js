031a2942c5d5698f465979d192fca63d7ae2e171
Stuart Verschoyle <stuart.verschoyle@vistaget.com>
2023-01-27 16:31:48 +0000

    add fleet title

---
+++ frontend/fleet/public/component---src-pages-index-js-070a1c990b07d25f7026.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfleet=self.webpackChunkfleet||[]).push([[678],{6558:function(e,t,n){n.r(t),n.d(t,{Head:function(){return a}});var l=n(7294);t.default=e=>{let{data:t}=e;const n=t.allSanityAnimal.edges.map((e=>l.createElement("div",{style:{backgroundColor:"#ddd",padding:"20px",margin:"20px 0"}},l.createElement("h2",null,e.node.name))));return l.createElement("main",null,l.createElement("h1",null,"Fleet"),n)};const a=()=>l.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-070a1c990b07d25f7026.js.map
--- frontend/fleet/src/pages/index.js
+++ frontend/fleet/src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = ({data}) => {
   ))
   return (
     <main>
-      <h1>Hi</h1> 
+      <h1>Fleet</h1> 
       {animals}
      </main>
   )
